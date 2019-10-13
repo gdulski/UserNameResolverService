33bf5b72457feb80ac7da6a447996ee1568afe0a
gdulski <g.dulski1@gmail.com>
2019-10-13 04:38:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-playground-mrxtnv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-playground-mrxtnv)
---
+++ index.ts
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const enum Shape {
+  Circle,
+  Square
+}
+
+console.log(Shape['1']);
